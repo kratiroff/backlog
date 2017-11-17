72f94baadb2cb4fc88f83af4949c48c57f2e3a23
kratiroff <hubert@kratiroff.com>
2017-11-17 18:49:54 +0100

    Parsing json

--- sync-redis.py
+++ sync-redis.py
@@ -1,11 +1,17 @@
 import redis
+import json
 
 r = redis.StrictRedis(host='213.32.89.6', port='7644', db=0)
 
 for key in r.scan_iter():
   try:
     value = r.hgetall(key)
-    print value
+    parsed = ''
+    if type(value) is str:
+      parsed = json.dumps(json.loads(value), sort_keys=True, indent=4)
+    else:
+      parsed = json.dumps(value, sort_keys=True, indent=4)
+    print parsed
     break
   except ValueError:
     print "Oops!"
